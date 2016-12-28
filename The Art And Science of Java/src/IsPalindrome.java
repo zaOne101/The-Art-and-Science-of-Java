4d12e3dcd354ba31e392dd415372a484e82fe854
Alexa <za0ne78@hotmail.com>
2016-12-28 11:18:36 +0200

    Create IsPalindrome.java

---
+++ The Art And Science of Java/src/IsPalindrome.java
@@ -0,0 +1,40 @@
+/*
+ * File: IsPalindrome.java
+ * ------------------------
+ * 2. Scrieti un program care verifica daca un numar este palindrom folosind metodele:
+ * // Return the reversal of an integer, i.e., reverse(456) returns 654
+ *      public static int reverse(int number)
+ * // Return true if number is a palindrome
+ *                             public static boolean isPalindrome(int number)
+ */
+
+
+
+import java.util.*;
+
+public class IsPalindrome {
+
+                 public static void main(String[] args) {
+                           		System. out.println("This program takes in a number, prints out it's reverse, then checks if the number is a Palindrome");
+                                Scanner input = new Scanner(System.in);
+                                System. out.println("Please enter an integer number: ");
+                                 int num = input.nextInt();
+                                System. out.println("The reverse number is " + reverse(num));
+                                System. out.println("Is the number a palindrome? "+ isPalindrome(num, reverse (num)));
+                                input.close();
+                 }
+                
+                 private static int reverse(int n) {
+                                 int reversed = 0;
+                                 while (n > 0) {
+                                                 reversed = reversed *10 + n % 10;
+                                                 n /= 10;
+                                 }
+                                 return reversed;
+                }
+                
+                 private static boolean isPalindrome( int n, int reversed) {
+                                 return n == reversed;
+                }
+
+}
